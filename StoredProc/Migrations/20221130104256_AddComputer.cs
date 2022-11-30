398d1e390e0139e1d82c0f5ed91eab8ac626b5f8
Alkems <mynamezalter@gmail.com>
2022-11-30 13:04:54 +0200

    Add Computer model, scaffold ComputerController and Add Migration.

---
+++ StoredProc/Migrations/20221130104256_AddComputer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoredProc.Migrations
+{
+    public partial class AddComputer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Computer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GPU = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPU = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Computer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Computer");
+        }
+    }
+}
---
+++ StoredProc/Models/Computer.cs
@@ -0,0 +1,11 @@
+﻿namespace StoredProc.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string GPU { get; set; }
+        public string CPU { get; set; }
+        public int Price { get; set; }
+    }
+}
